a4fcfa646f6803b3e164d219646f918238db3e90
Deepikagm18 <deepikagm18@gmail.com>
2021-03-18 13:16:26 +0530

    project done

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech {
+       private void greensOmr() {
+		// TODO Auto-generated method stub
+        System.out.println("Omr");
+	}
+       private void greensAdayar() {
+		// TODO Auto-generated method stub
+        System.out.println("Adayar");
+	}
+       private void greensTambaram() {
+		// TODO Auto-generated method stub
+        System.out.println("Tambaram");
+	}
+       private void greensvelacherry() {
+		// TODO Auto-generated method stub
+        System.out.println("Velacherry");
+	}
+       private void greensAnnaNagar() {
+		// TODO Auto-generated method stub
+        System.out.println("Anna nagar"); 
+			}
+       public static void main(String[] args) {
+    	   GreensTech gt=new GreensTech();
+    	   gt.greensOmr();
+    	   gt.greensAdayar();
+    	   gt.greensAnnaNagar();
+    	   gt.greensvelacherry();
+    	   gt.greensTambaram();
+    	   
+		
+	}
+}
